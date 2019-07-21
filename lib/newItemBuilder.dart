8e5a71f90c3d3a3e7ff0a644f8685264ae65ada7
jylee-git <jylee.w94@gmail.com>
2019-07-21 22:22:56 +0800

    Add itemName and basicPrice field

--- lib/newItemBuilder.dart
+++ lib/newItemBuilder.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class NewItemBuilder extends StatefulWidget {
-
   @override
   State createState() {
     return _NewItemBuilder();
@@ -9,29 +8,103 @@ class NewItemBuilder extends StatefulWidget {
 }
 
 class _NewItemBuilder extends State<NewItemBuilder> {
-  final _formKey = GlobalKey<FormState>();
+  final GlobalKey<FormState> _key = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
-    print('hellooooooooooooooo');
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
-          title: Text('New Item')
+          title: Text('New Item'),
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.check),
+                onPressed: () {
+                  setState(() {
+                    _key.currentState.validate();
+                    print('all ok');
+                  });
+                }),
+          ],
         ),
-      )
+        body: ListView(
+          key: Key('OrderedListView'),
+          children: <Widget>[
+            Form(
+              key: _key,
+              autovalidate: true,
+              child: _formBuilder(),
+            ),
+            Container(
+                child: RaisedButton(
+              child: Text('Add customizable options'),
+            )),
+          ],
+        ));
+  }
+
+  Widget _formBuilder() {
+    return Column(
+      children: <Widget>[
+        _getNonCustomizableInputWidget('Name of item'),
+        _getPriceInputWidget(),
+      ],
     );
-//    return ListView(
-//      children: <Widget>[
-//        TextFormField(
-//          validator: (value) {
-//            if (value.isEmpty) {
-//              return 'This field is required.';
-//            }
-//            return null;
-//          },
-//        )
-//      ],
-//    );
+  }
+
+  Widget _getPriceInputWidget() {
+    return Row(
+      children: <Widget>[
+        Container(
+          child: Text('Price: \$'),
+          padding: EdgeInsets.only(left: 10.0, right: 10.0),
+        ),
+        Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: Form(
+                child: TextFormField(
+                  autovalidate: true,
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'This field is required.';
+                    } else if (double.tryParse(value) == null) {
+                      print('bad');
+                      return 'Invalid input';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+            )),
+      ],
+    );
+  }
+
+  /// Returns title block.
+  Widget _getNonCustomizableInputWidget(String fieldName) {
+    return Row(
+      children: <Widget>[
+        Container(
+          child: Text(fieldName),
+          padding: EdgeInsets.only(left: 10.0, right: 10.0),
+        ),
+        Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: Form(
+                child: TextFormField(
+                  autovalidate: true,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'This field is required.';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+            ))
+      ],
+    );
   }
 }
